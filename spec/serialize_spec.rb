298febd92b34ce6b77e02f3a746ef81bda50288b
R. Tyler Croy <rtyler@brokenco.de>
2019-05-19 10:28:32 -0700

    Add a reproduction case for serialization errors with RSpec
    I :heart: Ruby

---
+++ spec/serialize_spec.rb
@@ -0,0 +1,33 @@
+require 'rspec'
+
+
+java_import 'org.apache.spark.sql.SparkSession'
+
+#
+# Inspired by test code found here:
+#  http://codewicca.org/the-inevitable-task-not-serializable-sparkexception
+
+describe 'Serializing Ruby for Spark' do
+  context 'with an object' do
+    let(:spark) do
+      SparkSession
+        .builder
+        .config('spark.serializer', 'org.apache.spark.serializer.KryoSerializer')
+        .master('local[*]')
+        .appName('rspec')
+        .getOrCreate
+    end
+
+    after(:each) do
+      spark.stop()
+    end
+
+    it 'should serialize and execute properly' do
+      data = spark.read.textFile(__FILE__).cache()
+
+      expect {
+        data.filter { |line| line.contains('a') }.count
+      }.not_to raise_error
+    end
+  end
+end
